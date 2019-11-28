f06867e2220f54be21a7ed98fc32d852231bb5d2
MacSpain <michal.t.kret@gmail.com>
2019-11-28 10:20:30 +0100

    another quick fix

--- src/map_streaming.c
+++ src/map_streaming.c
@@ -8,17 +8,15 @@ void OverwriteMap()
 	for (UWORD y = lastOverwrittenLine; y < p1y; y++)
 	{
 		yy = (UBYTE)y ;
+		yyplusone = yy+1;
 		mapNumber = (y / 256) % MAPLENGTH;
 		for (UWORD x = 0; x < MAPSIZE; x++)
 		{
 			word = mapSource[mapNumber][x][yy/2];
 			mapHigh[x*2][yy] = word;
 			mapHigh[x*2+1][yy] = word;
-			if(yy < 255)
-			{
-			mapHigh[x*2][yy+1] = word;
-			mapHigh[x*2+1][yy+1] = word;
-			}
+			mapHigh[x*2][yyplusone] = word;
+			mapHigh[x*2+1][yyplusone] = word;
 		}
 	}
 	lastOverwrittenLine = p1y;
